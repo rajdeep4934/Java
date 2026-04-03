874aeb931ac14b3f129455dde977ca4203f6e894
Rajdeep Dhondugade <118789019+rajdeep4934@users.noreply.github.com>
2026-04-03 16:11:41 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,41 @@
+ class AddNumbers{
+
+	public static void main(String[]args)
+	{
+	int c=0;
+	for(int i=0;i<args.length;i++)
+
+	{
+									
+	c+=Integer.parseInt(args[i]);
+
+		}
+
+	System.out.println("addition is  " +c);
+
+		}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
